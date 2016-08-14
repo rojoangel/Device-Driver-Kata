0f9dcb93e18d9f09cf19fac6c7434c1ac122cda1
Angel Rojo Marijuan <arojomarijuan@magento.com>
2016-08-14 13:16:02 +0200

    extract to internal private class write operation verification

--- Java/src/main/java/codekata/DeviceDriver.java
+++ Java/src/main/java/codekata/DeviceDriver.java
@@ -33,34 +33,7 @@ public class DeviceDriver {
     }
 
     private void verifyWrite(long address, byte data) throws WriteError {
-        byte readByte = waitForWriteOperationToComplete();
-        if (wasWriteOperationSuccessful(readByte)) {
-            if (!verifyWriteOperation(address, data)) {
-                throw new WriteVerificationError();
-            }
-        } else {
-            setHardwareReadyToAcceptNewWrites();
-            switch (readByte) {
-                case 0b0001000100:
-                    throw new VoltageError();
-                case 0b0001001000:
-                    throw new InternalHardwareError();
-                case 0b0001010000:
-                    throw new ProtectedBlockError();
-            }
-        }
-    }
-
-    private void setHardwareReadyToAcceptNewWrites() {
-        writeToHardware(0x0, (byte) 0xFF);
-    }
-
-    private boolean verifyWriteOperation(long address, byte data) {
-        return readFromHardware(address) == data;
-    }
-
-    private boolean wasWriteOperationSuccessful(byte readByte) {
-        return readByte == 0b0001000000;
+        new WriteOperationVerifier().verify(address, data);
     }
 
     private byte readFromHardware(long address) {
@@ -69,20 +42,6 @@ public class DeviceDriver {
 
     private void writeToHardware(long address, byte data) {
         getHardware().write(address, data);
-    }
-
-    private byte waitForWriteOperationToComplete() throws ReadyBitTimeoutError{
-        byte readByte;
-        do {
-            readByte = readFromHardware(0x0);
-            if (isReadyBitSet(readByte)) {
-                break;
-            }
-            if (timeout()) {
-                throw new ReadyBitTimeoutError();
-            }
-        } while (true);
-        return readByte;
     }
 
     private boolean timeout() {
@@ -95,5 +54,53 @@ public class DeviceDriver {
 
     private void writeProgramCommand() {
         writeToHardware(0x0, PROGRAM_COMMAND);
+    }
+
+    private class WriteOperationVerifier {
+
+        public void verify(long address, byte data) throws WriteError {
+            byte readByte = waitForWriteOperationToComplete();
+            if (wasWriteOperationSuccessful(readByte)) {
+                if (!verifyWriteOperation(address, data)) {
+                    throw new WriteVerificationError();
+                }
+            } else {
+                setHardwareReadyToAcceptNewWrites();
+                switch (readByte) {
+                    case 0b0001000100:
+                        throw new VoltageError();
+                    case 0b0001001000:
+                        throw new InternalHardwareError();
+                    case 0b0001010000:
+                        throw new ProtectedBlockError();
+                }
+            }
+        }
+
+        private byte waitForWriteOperationToComplete() throws ReadyBitTimeoutError{
+            byte readByte;
+            do {
+                readByte = readFromHardware(0x0);
+                if (isReadyBitSet(readByte)) {
+                    break;
+                }
+                if (timeout()) {
+                    throw new ReadyBitTimeoutError();
+                }
+            } while (true);
+            return readByte;
+        }
+
+        private boolean wasWriteOperationSuccessful(byte readByte) {
+            return readByte == 0b0001000000;
+        }
+
+        private boolean verifyWriteOperation(long address, byte data) {
+            return readFromHardware(address) == data;
+        }
+
+        private void setHardwareReadyToAcceptNewWrites() {
+            writeToHardware(0x0, (byte) 0xFF);
+        }
     }
 }
