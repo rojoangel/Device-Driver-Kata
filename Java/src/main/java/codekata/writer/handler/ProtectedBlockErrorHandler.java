7544c791ddeb53aab23d479aa06a1ca7232dd584
Angel Rojo Marijuan <arojomarijuan@magento.com>
2016-08-14 22:18:39 +0200

    change ProtectedBlockErrorHandler to use HardwareResetter

--- Java/src/main/java/codekata/writer/handler/ProtectedBlockErrorHandler.java
+++ Java/src/main/java/codekata/writer/handler/ProtectedBlockErrorHandler.java
@@ -3,20 +3,18 @@ package codekata.writer.handler;
 import codekata.FlashMemoryDevice;
 import codekata.exception.WriteError;
 import codekata.exception.write.ProtectedBlockError;
+import codekata.writer.HardwareResetter;
 import codekata.writer.WriteErrorHandler;
 
 public class ProtectedBlockErrorHandler implements WriteErrorHandler {
-    private FlashMemoryDevice hardware;
+    private final HardwareResetter hardwareResetter;
 
     public ProtectedBlockErrorHandler(FlashMemoryDevice hardware) {
-        this.hardware = hardware;
+        this.hardwareResetter = new HardwareResetter(hardware);
     }
 
     public void handle() throws WriteError {
-        setHardwareReadyToAcceptNewWrites();
+        hardwareResetter.reset();
         throw new ProtectedBlockError();
-    }
-    private void setHardwareReadyToAcceptNewWrites() {
-        hardware.write((long) 0x0, (byte) 0xFF);
     }
 }
