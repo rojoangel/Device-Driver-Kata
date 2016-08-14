3caf70e72b575192f7677acb4336e611b9a7ce8d
Angel Rojo Marijuan <arojomarijuan@magento.com>
2016-08-14 13:35:09 +0200

    extract WriteOperationVerifier private class to public class

--- Java/src/main/java/codekata/DeviceDriver.java
+++ Java/src/main/java/codekata/DeviceDriver.java
@@ -1,7 +1,6 @@
 package codekata;
 
 import codekata.exception.WriteError;
-import codekata.exception.write.*;
 
 /**
  * This class is used by the operating system to interact with the hardware 'FlashMemoryDevice'.
@@ -21,7 +20,7 @@ public class DeviceDriver {
     public DeviceDriver(FlashMemoryDevice hardware, Timer timer) {
         this.hardware = hardware;
         this.timer = timer;
-        this.writeVerifier = new WriteOperationVerifier();
+        this.writeVerifier = new WriteOperationVerifier(hardware, timer);
     }
 
     public byte read(long address) {
@@ -38,10 +37,6 @@ public class DeviceDriver {
         this.writeVerifier.verify(address, data);
     }
 
-    private byte readFromHardware(long address) {
-        return getHardware().read(address);
-    }
-
     private void writeToHardware(long address, byte data) {
         getHardware().write(address, data);
     }
@@ -50,59 +45,4 @@ public class DeviceDriver {
         writeToHardware(0x0, PROGRAM_COMMAND);
     }
 
-    private class WriteOperationVerifier {
-
-        public void verify(long address, byte data) throws WriteError {
-            byte readByte = waitForWriteOperationToComplete();
-            if (wasWriteOperationSuccessful(readByte)) {
-                if (!verifyWrittenData(address, data)) {
-                    throw new WriteVerificationError();
-                }
-            } else {
-                setHardwareReadyToAcceptNewWrites();
-                switch (readByte) {
-                    case 0b0001000100:
-                        throw new VoltageError();
-                    case 0b0001001000:
-                        throw new InternalHardwareError();
-                    case 0b0001010000:
-                        throw new ProtectedBlockError();
-                }
-            }
-        }
-
-        private byte waitForWriteOperationToComplete() throws ReadyBitTimeoutError{
-            byte readByte;
-            do {
-                readByte = readFromHardware(0x0);
-                if (isReadyBitSet(readByte)) {
-                    break;
-                }
-                if (timeout()) {
-                    throw new ReadyBitTimeoutError();
-                }
-            } while (true);
-            return readByte;
-        }
-
-        private boolean wasWriteOperationSuccessful(byte readByte) {
-            return readByte == 0b0001000000;
-        }
-
-        private boolean isReadyBitSet(byte readByte) {
-            return (readByte & 0b0001000000) == 0b0001000000;
-        }
-
-        private boolean verifyWrittenData(long address, byte data) {
-            return readFromHardware(address) == data;
-        }
-
-        private void setHardwareReadyToAcceptNewWrites() {
-            writeToHardware(0x0, (byte) 0xFF);
-        }
-
-        private boolean timeout() {
-            return timer.hasTimedOut();
-        }
-    }
 }
---
+++ Java/src/main/java/codekata/WriteOperationVerifier.java
@@ -0,0 +1,79 @@
+package codekata;
+
+import codekata.exception.WriteError;
+import codekata.exception.write.*;
+
+class WriteOperationVerifier {
+
+    private FlashMemoryDevice hardware;
+    private Timer timer;
+
+    WriteOperationVerifier(FlashMemoryDevice hardware, Timer timer) {
+        this.hardware = hardware;
+        this.timer = timer;
+    }
+
+    public void verify(long address, byte data) throws WriteError {
+        byte readByte = waitForWriteOperationToComplete();
+        if (wasWriteOperationSuccessful(readByte)) {
+            if (!verifyWrittenData(address, data)) {
+                throw new WriteVerificationError();
+            }
+        } else {
+            setHardwareReadyToAcceptNewWrites();
+            switch (readByte) {
+                case 0b0001000100:
+                    throw new VoltageError();
+                case 0b0001001000:
+                    throw new InternalHardwareError();
+                case 0b0001010000:
+                    throw new ProtectedBlockError();
+            }
+        }
+    }
+
+    private byte waitForWriteOperationToComplete() throws ReadyBitTimeoutError {
+        byte readByte;
+        do {
+            readByte = readFromHardware(0x0);
+            if (isReadyBitSet(readByte)) {
+                break;
+            }
+            if (timeout()) {
+                throw new ReadyBitTimeoutError();
+            }
+        } while (true);
+        return readByte;
+    }
+
+    private boolean wasWriteOperationSuccessful(byte readByte) {
+        return readByte == 0b0001000000;
+    }
+
+    private boolean isReadyBitSet(byte readByte) {
+        return (readByte & 0b0001000000) == 0b0001000000;
+    }
+
+    private boolean verifyWrittenData(long address, byte data) {
+        return readFromHardware(address) == data;
+    }
+
+    private void setHardwareReadyToAcceptNewWrites() {
+        writeToHardware(0x0, (byte) 0xFF);
+    }
+
+    private boolean timeout() {
+        return timer.hasTimedOut();
+    }
+
+    private byte readFromHardware(long address) {
+        return getHardware().read(address);
+    }
+    private void writeToHardware(long address, byte data) {
+        getHardware().write(address, data);
+    }
+    private FlashMemoryDevice getHardware() {
+        return hardware;
+    }
+
+}
