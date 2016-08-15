41d4a09bfe6153d121d1bf3f0e2b56379b439875
Angel Rojo Marijuan <arojomarijuan@magento.com>
2016-08-15 22:23:49 +0200

    remove unused imports

--- Java/src/test/java/codekata/writer/handler/InternalHardwareErrorHandlerTest.java
+++ Java/src/test/java/codekata/writer/handler/InternalHardwareErrorHandlerTest.java
@@ -8,8 +8,6 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
 public class InternalHardwareErrorHandlerTest {
 
     private Mockery context;
--- Java/src/test/java/codekata/writer/handler/ProtectedBlockErrorHandlerTest.java
+++ Java/src/test/java/codekata/writer/handler/ProtectedBlockErrorHandlerTest.java
@@ -8,8 +8,6 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
 public class ProtectedBlockErrorHandlerTest {
 
     private Mockery context;
