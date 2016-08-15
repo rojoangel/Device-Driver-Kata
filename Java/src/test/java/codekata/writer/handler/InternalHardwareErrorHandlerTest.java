9ef391e2247a135704bb2b4c885599641815eb57
Angel Rojo Marijuan <arojomarijuan@magento.com>
2016-08-15 22:13:49 +0200

    add InternalHardwareErrorHandlerTest

---
+++ Java/src/test/java/codekata/writer/handler/InternalHardwareErrorHandlerTest.java
@@ -0,0 +1,38 @@
+package codekata.writer.handler;
+
+import codekata.FlashMemoryDevice;
+import codekata.exception.write.InternalHardwareError;
+import org.jmock.Expectations;
+import org.jmock.Mockery;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class InternalHardwareErrorHandlerTest {
+
+    private Mockery context;
+
+    @Before
+    public void setUp() throws Exception {
+        context = new Mockery();
+
+    }
+
+    @Test(expected = InternalHardwareError.class)
+    public void testHandle() throws Exception {
+        FlashMemoryDevice hardware = context.mock(FlashMemoryDevice.class);
+        context.checking(new Expectations() {{
+            oneOf(hardware).write((long) 0x0, (byte) 0xFF);
+        }});
+
+        InternalHardwareErrorHandler handler = new InternalHardwareErrorHandler(hardware);
+        handler.handle();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        context.assertIsSatisfied();
+    }
+}
